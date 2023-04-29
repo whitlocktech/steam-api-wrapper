afa422b1a34aa65c4fdf9e0871ffef3a3d4191fe
whitlocktech <whitlocktech@gmail.com>
2023-04-28 23:39:26 -0600

    Expand api calls (#3)
    * added news
    
    * Added query endpoints
    
    * Added endpoints Manually tested
    
    * updated version number

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is a wrapper for the steam api. I am working to add more functionality to it.
 
+The getMultipeProfiles function is currently non operational. Other functions might retrun an error if the user has a private profile
+
 To Add it to your project
 
 npm i @whitlocktech/steam-api-wrapper
@@ -30,3 +32,47 @@ getMultipleProfiles(steamApiKey, steamIds);
 ```
 
 steamIds is passed in as an array. The function will join and then request multiple user profiles.
+
+```javascript
+getNewsForApp(appId, count, maxLength);
+```
+
+You must pass in the appId which is the id that steam uses to identify games, the count of the articles, and the maxLength of characters for the articles.
+count defaults to 3 if not passed in
+maxLength defaults to 300
+
+```javascript
+getGlobalAchievementPercentageForApp(appId);
+```
+
+appId is the id used by steam for apps. It will return the global percentages of
+acheivemnts
+
+```javascript
+getFriendList(steaApiKey, steamId);
+```
+
+Requires your steam API key and the 64bit steamId of the person you wish to get the friends list of
+
+```javascript
+getPlayerAchievements(appId, steamApiKey, steamId);
+```
+
+This is a v0001 api call maybe deperacated in the future
+get the achievements for a single user for specific game
+Requires appId = steam app id
+steamApiKey your steam api key
+64bit steamId of the user you are targeting
+
+```javascript
+getUserStatsForGame(appId, steamApiKey, steamId);
+```
+
+This returns the same data and has the same getPlayerAchievements function but it is the v0002(current) steam api call
+
+```javascript
+getOwnedGames(steamApiKey, steamId);
+```
+
+This get the owned games list of the targeted player
+requires your steam api key, and the 64bit steam id of the player you are targeting.
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const axios = require('axios');
+/* Code used for manual testing only not needed
+require('dotenv').config()
 
+const steamApiKey = process.env.STEAM_API_KEY
+const steamId = '76561198038695920'
+const steamIds = process.env.STEAM_IDS
+const appId = '252490'
+const count = 3
+const maxLength = 300
+*/
 async function getProfile(steamApiKey, steamId) {
     try {
         const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamApiKey}&steamids=${steamId}&format=json`);
@@ -10,19 +19,87 @@ async function getProfile(steamApiKey, steamId) {
     }
 }
 
-async function getMultipleProfiles(steamApiKey, steamIds) {
-    const steamIDS = steamIds.join(',')
-    console.log(steamIDS)
+// async function getMultipleProfiles(steamApiKey, steamIds) {
+//     const steamIDS = steamIds.join(',')
+//     console.log(steamIDS)
+//     try {
+//         const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamApiKey}&steamids=${steamIDS}&format=json`)
+//         console.log(response.data.response.players)
+//         return response.data.response.players
+//     } catch (error) {
+//         console.error(error)
+//     }
+// }
+
+async function getNewsForApp(appId, count, maxLength) {
+    newsCount = count || 3
+    totalLength = maxLength || 300
+    console.log(appId)
     try {
-        const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamApiKey}&steamids=${steamIDS}&format=json`)
-        console.log(response.data.response.players)
-        return response.data.response.players
+        const response = await axios.get(`http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${appId}&count=${count}&maxlength=${maxLength}&format=json`)
+        console.log(response.data.appnews)
+        return response.data.appnews
     } catch (error) {
         console.error(error)
     }
 }
 
+async function getGlobalAchievementPercentageForApp(appId) {
+    try {
+        const response = await axios.get(`https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=${appId}&format=json`)
+        console.log(response.data.achievementpercentages)
+        return response.data.achievementpercentages
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function getFriendList(steamApiKey, steamId) {
+    try {
+        const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=${steamApiKey}&steamid=${steamId}&relationship=friend&format=json`)
+        console.log(response)
+        return response.data.friendslist.friends
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function getPlayerAchievements(appId, steamApiKey, steamId) {
+    try {
+        const response = await axios.get(`https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${appId}&key=${steamApiKey}&steamid=${steamId}&format=json`)
+        console.log(response.data.playerstats)
+        return response.playerstats.achievements
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function getUserStatsForGame(appId, steamApiKey, steamId) {
+    try {
+        const response = await axios.get(`http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=${appId}&key=${steamApiKey}&steamid=${steamId}`)
+        console.log(response.data)
+        return response.playerstats.achievements
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function getOwnedGames(steamApiKey, steamId) {
+    try {
+        const response = await axios.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamApiKey}&steamid=${steamId}&format=json`)
+        console.log(response.response.games)
+        return response.response.games
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 module.exports = {
     getProfile,
-    getMultipleProfiles
+    getNewsForApp,
+    getGlobalAchievementPercentageForApp,
+    getFriendList,
+    getPlayerAchievements,
+    getUserStatsForGame,
+    getOwnedGames
 }
